ae4ad6d9cb4129d720162c33652dd38fd4557db3
Lucas Pereira <Lucas Pereira@Pereira>
2020-10-28 21:32:35 -0300

    Adicionando imagem na tela Cadastro de veiculos

--- src/main/java/br/com/senac/view/TelaCadastroVeiculo.java
+++ src/main/java/br/com/senac/view/TelaCadastroVeiculo.java
@@ -17,6 +17,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
 import javax.swing.UIManager;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextArea;
 
 public class TelaCadastroVeiculo extends JFrame {
 
@@ -50,7 +57,7 @@ public class TelaCadastroVeiculo extends JFrame {
 	public TelaCadastroVeiculo() {
 		setTitle("Cadastro de Veiculo");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 613, 390);
+		setBounds(100, 100, 604, 421);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(240, 248, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -87,35 +94,35 @@ public class TelaCadastroVeiculo extends JFrame {
 		contentPane.add(lblDadosVeiculo);
 		
 		TextField txtMarca = new TextField();
-		txtMarca.setBounds(56, 163, 146, 22);
+		txtMarca.setBounds(56, 160, 146, 22);
 		contentPane.add(txtMarca);
 		
 		JLabel lblMarca = new JLabel("Marca");
-		lblMarca.setBounds(56, 148, 46, 14);
+		lblMarca.setBounds(56, 145, 46, 14);
 		contentPane.add(lblMarca);
 		
 		TextField txtModelo = new TextField();
-		txtModelo.setBounds(223, 163, 146, 22);
+		txtModelo.setBounds(223, 160, 146, 22);
 		contentPane.add(txtModelo);
 		
 		JLabel lblModelo = new JLabel("Modelo");
-		lblModelo.setBounds(223, 148, 46, 14);
+		lblModelo.setBounds(223, 145, 46, 14);
 		contentPane.add(lblModelo);
 		
 		TextField txtAno = new TextField();
-		txtAno.setBounds(388, 163, 76, 22);
+		txtAno.setBounds(388, 160, 76, 22);
 		contentPane.add(txtAno);
 		
 		JLabel lblAno = new JLabel("Ano");
-		lblAno.setBounds(388, 148, 46, 14);
+		lblAno.setBounds(388, 145, 46, 14);
 		contentPane.add(lblAno);
 		
 		JLabel lblCor = new JLabel("Cor");
-		lblCor.setBounds(470, 148, 46, 14);
+		lblCor.setBounds(470, 145, 46, 14);
 		contentPane.add(lblCor);
 		
 		TextField txtCor = new TextField();
-		txtCor.setBounds(470, 163, 64, 22);
+		txtCor.setBounds(470, 160, 64, 22);
 		contentPane.add(txtCor);
 		
 		chkArCondicionado = new JCheckBox("Ar condicionado");
@@ -125,17 +132,57 @@ public class TelaCadastroVeiculo extends JFrame {
 		
 		JCheckBox chkDirecaoHidraulica = new JCheckBox("Dire\u00E7\u00E3o Hidraulica");
 		chkDirecaoHidraulica.setBackground((Color) null);
-		chkDirecaoHidraulica.setBounds(195, 250, 129, 23);
+		chkDirecaoHidraulica.setBounds(217, 250, 129, 23);
 		contentPane.add(chkDirecaoHidraulica);
 		
 		JCheckBox chkVidroEletrico = new JCheckBox("Vidro El\u00E9trico");
 		chkVidroEletrico.setBackground((Color) null);
-		chkVidroEletrico.setBounds(466, 250, 111, 23);
+		chkVidroEletrico.setBounds(387, 276, 111, 23);
 		contentPane.add(chkVidroEletrico);
 		
 		JCheckBox chkDirecaoEletrica = new JCheckBox("Dire\u00E7\u00E3o El\u00E9trica");
 		chkDirecaoEletrica.setBackground((Color) null);
-		chkDirecaoEletrica.setBounds(335, 250, 123, 23);
+		chkDirecaoEletrica.setBounds(387, 250, 123, 23);
 		contentPane.add(chkDirecaoEletrica);
+		
+		JCheckBox chkDuasPortas = new JCheckBox("2 Portas");
+		chkDuasPortas.setBackground((Color) null);
+		chkDuasPortas.setBounds(56, 276, 123, 23);
+		contentPane.add(chkDuasPortas);
+		
+		JCheckBox chkQuatroPortas = new JCheckBox("4 Portas");
+		chkQuatroPortas.setBackground((Color) null);
+		chkQuatroPortas.setBounds(217, 276, 123, 23);
+		contentPane.add(chkQuatroPortas);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(52, 241, 482, 2);
+		contentPane.add(separator);
+		
+		JButton btnSalvar = new JButton(" Salvar");
+		btnSalvar.setHorizontalAlignment(SwingConstants.LEFT);
+		btnSalvar.setIcon(new ImageIcon(TelaCadastroVeiculo.class.getResource("/icons/Salvar.png")));
+		btnSalvar.setForeground(new Color(0, 0, 139));
+		btnSalvar.setBackground(new Color(240, 248, 255));
+		btnSalvar.setBounds(223, 330, 111, 41);
+		contentPane.add(btnSalvar);
+		
+		TextField txtMotor = new TextField();
+		txtMotor.setBounds(56, 214, 146, 22);
+		contentPane.add(txtMotor);
+		
+		JLabel lblMotor = new JLabel("Motor");
+		lblMotor.setBounds(56, 199, 76, 14);
+		contentPane.add(lblMotor);
+		
+		JLabel lblTransmissao = new JLabel("Transmiss\u00E3o");
+		lblTransmissao.setBounds(223, 199, 88, 14);
+		contentPane.add(lblTransmissao);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setToolTipText("");
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"            Selecione", "Autom\u00E1tica", "Manual"}));
+		comboBox.setBounds(223, 214, 146, 22);
+		contentPane.add(comboBox);
 	}
 }
