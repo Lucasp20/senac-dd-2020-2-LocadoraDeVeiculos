9f4b217088b7cbcfeca0ac544aaf697886f238fd
Lucas Pereira <Lucas Pereira@Pereira>
2020-11-09 19:55:34 -0300

    Criando a classe banco

---
+++ src/main/java/br/com/senac/view/Executavel.java
@@ -0,0 +1,16 @@
+package br.com.senac.view;
+
+import br.com.senac.model.dao.Banco;
+
+public class Executavel {
+
+	public static void main(String[] args) {
+		
+		
+		/* Teste de conexão com o banco 
+	
+		Banco.getConnection();
+		 System.out.println("OK"); */
+	}
+
+}
